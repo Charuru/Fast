a5428aca6a6394f5d2427670415283bb1d3435df
Charles <charuru2009@gmail.com>
2014-08-22 19:03:50 -0400

    Moar files and concats

--- gulpfile.js
+++ gulpfile.js
@@ -2,48 +2,86 @@
 // For production  => gulp -p
 
 // Call Plugins
-var env        = require('minimist')(process.argv.slice(2)),
-	gulp       = require('gulp'),
-	gutil      = require('gulp-util'),
-	jade       = require('gulp-jade'),
+var env = require('minimist')(process.argv.slice(2)),
+	gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	jade = require('gulp-jade'),
+	concat = require('gulp-concat'),
 	browserify = require('gulp-browserify'),
-	uglify     = require('gulp-uglify'),
-	gulpif     = require('gulp-if'),
-	stylus     = require('gulp-stylus'),
-	jeet       = require('jeet'),
-	rupture    = require('rupture'),
-	axis       = require('axis-css'),
-	prefixer   = require('autoprefixer-stylus'),
-	connect    = require('gulp-connect'),
+	uglify = require('gulp-uglify'),
+	gulpif = require('gulp-if'),
+	stylus = require('gulp-stylus'),
+	jeet = require('jeet'),
+	rupture = require('rupture'),
+	axis = require('axis-css'),
+	prefixer = require('autoprefixer-stylus'),
+	connect = require('gulp-connect'),
 	modRewrite = require('connect-modrewrite'),
-	imagemin   = require('gulp-imagemin'),
-	karma      = require('gulp-karma'),
-	rsync      = require('rsyncwrapper').rsync;
+	imagemin = require('gulp-imagemin'),
+	karma = require('gulp-karma'),
+	rsync = require('rsyncwrapper').rsync;
 
 // Call Jade for compile Templates
-gulp.task('jade', function(){
-	return gulp.src('src/templates/*.jade')
-		.pipe(jade({pretty: !env.p }))
+gulp.task('jade', function() {
+	return gulp.src('src/tmpl/**/*.jade')
+		.pipe(jade({
+			pretty: !env.p,
+			client: true
+		}))
+		.pipe(gulp.dest('build/tmpl/'))
+		.pipe(connect.reload());
+});
+
+// jade.index
+gulp.task('index', function() {
+	return gulp.src('src/index.jade')
+		.pipe(jade({
+			pretty: !env.p
+		}))
 		.pipe(gulp.dest('build/'))
 		.pipe(connect.reload());
 });
 
 // Call Uglify and Concat JS
-gulp.task('js', function(){
-	return gulp.src('src/js/main.js')
-		.pipe(browserify({debug: !env.p }))
+gulp.task('js', function() {
+	return gulp.src('src/js/**/*.js')
+		// .pipe(browserify({
+		// 	debug: !env.p
+		// }))
 		.pipe(gulpif(env.p, uglify()))
+		.pipe(concat('main.js'))
 		.pipe(gulp.dest('build/js'))
 		.pipe(connect.reload());
 });
 
+gulp.task('vendorjs', function() {
+	return gulp.src('src/vendor/**/*.js')
+		// .pipe(browserify({
+		// 	debug: !env.p
+		// }))
+		.pipe(gulpif(env.p, uglify()))
+		.pipe(concat('vendor.js'))
+		.pipe(gulp.dest('build/js'))
+		.pipe(connect.reload());
+});
+
+
+gulp.task('vendorcss', function() {
+	return gulp.src('src/vendor/**/*.css')
+		.pipe(concat('vendor.css'))
+		.pipe(gulp.dest('build/css'))
+		.pipe(connect.reload());
+});
+
+
 // Call Stylus
-gulp.task('stylus', function(){
-		gulp.src('src/styl/main.styl')
+gulp.task('stylus', function() {
+	gulp.src('src/styl/**/*.styl')
 		.pipe(stylus({
-			use:[axis(), prefixer(), jeet(),rupture()],
+			use: [axis(), prefixer(), jeet(), rupture()],
 			compress: env.p
 		}))
+		.pipe(concat('main.css'))
 		.pipe(gulp.dest('build/css'))
 		.pipe(connect.reload());
 });
@@ -51,12 +89,16 @@ gulp.task('stylus', function(){
 // Call Imagemin
 gulp.task('imagemin', function() {
 	return gulp.src('src/img/**/*')
-		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+		.pipe(imagemin({
+			optimizationLevel: 3,
+			progressive: true,
+			interlaced: true
+		}))
 		.pipe(gulp.dest('build/img'));
 });
 
 // Call Watch
-gulp.task('watch', function(){
+gulp.task('watch', function() {
 	gulp.watch('src/templates/**/*.jade', ['jade']);
 	gulp.watch('src/styl/**/*.styl', ['stylus']);
 	gulp.watch('src/js/**/*.js', ['js']);
@@ -68,7 +110,7 @@ gulp.task('connect', function() {
 	connect.server({
 		root: ['build/'],
 		livereload: true,
-		middleware: function(){
+		middleware: function() {
 			return [
 				modRewrite([
 					'^/$ /index.html',
@@ -80,21 +122,21 @@ gulp.task('connect', function() {
 });
 
 // Rsync
-gulp.task('deploy', function(){
+gulp.task('deploy', function() {
 	rsync({
-		ssh: true,
-		src: './build/',
-		dest: 'user@hostname:/path/to/www',
-		recursive: true,
-		syncDest: true,
-		args: ['--verbose']
-	},
-		function (erro, stdout, stderr, cmd) {
+			ssh: true,
+			src: './build/',
+			dest: 'user@hostname:/path/to/www',
+			recursive: true,
+			syncDest: true,
+			args: ['--verbose']
+		},
+		function(erro, stdout, stderr, cmd) {
 			gutil.log(stdout);
-	});
+		});
 });
 
 // Default task
-gulp.task('default', ['js', 'jade', 'stylus', 'imagemin', 'watch', 'connect']);
+gulp.task('default', ['index', 'vendorcss', 'vendorjs', 'js', 'jade', 'stylus', 'imagemin', 'watch', 'connect']);
 // Build and Deploy
-gulp.task('build', ['js', 'jade', 'stylus', 'imagemin', 'deploy']);
+gulp.task('build', ['index', 'vendorcss', 'vendorjs', 'js', 'jade', 'stylus', 'imagemin', 'deploy']);
